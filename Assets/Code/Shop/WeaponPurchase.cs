807623df4fa55d71bf3ca75ae92f59db59af6806
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-03-24 15:57:45 -0500

    added class structure for shop ui

---
+++ Assets/Code/Shop/WeaponPurchase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPurchase : UpgradeBase
+{
+    protected override void ApplyUpgrade()
+    {
+        // Add the health upgrade effect.
+        // ...
+    }
+}
